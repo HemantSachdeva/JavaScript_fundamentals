34f34bd89e58ed6b500a185b21460cb388e499b3
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-05-01 23:31:09 +0530

    task06: Add two numbers 10 & 20 using variable
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ task06/task06.js
@@ -0,0 +1,13 @@
+function task06(){
+    // Use variable x and y for storing data
+    // Use variable result for storing sum of previous variable
+    // Enter your logic here
+    var x = 10;
+    var y = 20;
+    var sum = x + y;
+    console.log(sum);
+    // Do not edit/update below statements. (For system use only)
+    return sum
+}
+
+module.exports = task06
