14440152464f0d3f96a5569ba5dff3312797b8a8
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-05-01 23:30:36 +0530

    task05: Program to store 18 in a variable and print it, then update it's value to 19
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ task05/task05.js
@@ -0,0 +1,16 @@
+function task05(){
+
+    // Use x as your variable
+    // Enter your logic here
+
+    var x = 18;
+    console.log(x);
+
+    x = 19;
+    console.log(x);
+
+    // Do not edit/update below statements. (Only for system use)
+    return x;
+}
+
+module.exports = task05
